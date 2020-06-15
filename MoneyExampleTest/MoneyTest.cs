bfa57342d3de5a6e77b6d973dff259727ff263a0
naichilab <naichilab@live.jp>
2020-06-16 01:53:59 +0900

    p78 不要になったDollerとFrancを削除(GREEN)

--- MoneyExampleTest/MoneyTest.cs
+++ MoneyExampleTest/MoneyTest.cs
@@ -19,17 +19,7 @@ namespace MoneyExampleTest
         {
             Assert.True(Money.Dollar(5).Equals(Money.Dollar(5)));
             Assert.False(Money.Dollar(5).Equals(Money.Dollar(6)));
-            Assert.True(Money.Franc(5).Equals(Money.Franc(5)));
-            Assert.False(Money.Franc(5).Equals(Money.Franc(6)));
             Assert.False(Money.Franc(5).Equals(Money.Dollar(5)));
-        }
-
-        [Fact]
-        public void TestFrancMultiplication()
-        {
-            Money five = Money.Franc(5);
-            Assert.Equal(Money.Franc(10), five.Times(2));
-            Assert.Equal(Money.Franc(15), five.Times(3));
         }
 
         [Fact]
@@ -37,12 +27,6 @@ namespace MoneyExampleTest
         {
             Assert.Equal("USD", Money.Dollar(1).Currency());
             Assert.Equal("CHF", Money.Franc(1).Currency());
-        }
-
-        [Fact]
-        public void TestDifferentClassEquality()
-        {
-            Assert.True(new Money(10, "CHF").Equals(new Franc(10, "CHF")));
         }
     }
 }
