8f913dc2ef351373f8439a91ed3a87af470188a5
naichilab <naichilab@live.jp>
2020-06-16 01:07:27 +0900

    p38 EqualsメソッドをMoneyに移動

--- MoneyExample/Dollar.cs
+++ MoneyExample/Dollar.cs
@@ -14,12 +14,5 @@ namespace MoneyExample
             return new Dollar(Amount * multiplier);
         }
 
-        public override bool Equals(object? obj)
-        {
-            if (obj == null) return false;
-            var other = obj as Dollar;
-            if (other == null) return false;
-            return Amount == other.Amount;
-        }
     }
 }
--- MoneyExample/Money.cs
+++ MoneyExample/Money.cs
@@ -3,5 +3,11 @@ namespace MoneyExample
     public class Money
     {
         protected int Amount;
+
+        public override bool Equals(object? obj)
+        {
+            Money money = (Money) obj;
+            return Amount == money.Amount;
+        }
     }
 }
