75eb03c4c1bfc31153b857ac88bf3ecf17bd28d3
naichilab <naichilab@live.jp>
2020-06-16 00:56:32 +0900

    p31 Francクラスをコピペ追加(GREEN)

---
+++ MoneyExample/Franc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MoneyExample
+{
+    public class Franc
+    {
+        private readonly int _amount;
+
+        public Franc(int amount)
+        {
+            _amount = amount;
+        }
+
+        public Franc Times(int multiplier)
+        {
+            return new Franc(_amount * multiplier);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            var other = obj as Franc;
+            if (other == null) return false;
+            return _amount == other._amount;
+        }
+    }
+}
