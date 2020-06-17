7cfca8c0b181f92a8480bd030560d72c4245a0cd
naichilab <naichilab@live.jp>
2020-06-18 01:56:56 +0900

    p94 MoneyをReduce

--- MoneyExample/Bank.cs
+++ MoneyExample/Bank.cs
@@ -4,6 +4,11 @@ namespace MoneyExample
     {
         public Money Reduce(Expression source, string to)
         {
+            if (source is Money)
+            {
+                return (Money) source;
+            }
+
             Sum sum = (Sum) source;
             return sum.Reduce(to);
         }
