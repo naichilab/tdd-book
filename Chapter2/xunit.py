6dd8697f4b5f47e8c0c0aee780a96a61dbd278d0
naichilab <naichilab@live.jp>
2020-06-19 14:40:45 +0900

    整形

--- Chapter2/xunit.py
+++ Chapter2/xunit.py
@@ -1,10 +1,13 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+
     def setUp(self):
         pass
+
     def tearDown(self):
         pass
+
     def run(self):
         result = TestResult()
         result.testStarted()
@@ -14,35 +17,45 @@ class TestCase:
         self.tearDown()
         return result
 
+
 class TestResult:
     def __init__(self):
         self.runCount = 0
+
     def testStarted(self):
         self.runCount = self.runCount + 1
+
     def summary(self):
         return "%d run, 0 failed" % self.runCount
+
 
 class WasRun(TestCase):
     def setUp(self):
         self.log = "setUp "
+
     def testMethod(self):
         self.log = self.log + "testMethod "
+
     def tearDown(self):
         self.log = self.log + "tearDown "
+
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
-        assert("setUp testMethod tearDown " == test.log)
+        assert ("setUp testMethod tearDown " == test.log)
+
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
         result = test.run()
         assert ("1 run, 1 failed" == result.summary())
+
     def testResult(self):
         test = WasRun("testMethod")
         result = test.run()
-        assert("1 run, 0 failed" == result.summary())
+        assert ("1 run, 0 failed" == result.summary())
+
 
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
